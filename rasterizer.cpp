d5097349e4ccab848290aece9d3dc6d3856f0450
z78406 <zzyofficetime@gmail.com>
2022-04-06 13:15:05 +0900

    latest commit

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -33,6 +33,7 @@ void rst::rasterizer::drawLine(Eigen::Vector3f begin, Eigen::Vector3f end, Eigen
 	float error = 0;
 	const int yincrs = y2 > y1? 1: -1; // increase / derease of y
 	Eigen::Vector2i point = Eigen::Vector2i; // next line point position
+	int y = y1;
 
 	if (steep) {		
 		for (int x = x1; x <= x2; x++) {
@@ -61,8 +62,61 @@ void rst::rasterizer::drawLine(Eigen::Vector3f begin, Eigen::Vector3f end, Eigen
 	}
 }
 
-void rst::rasterizer::drawLine_midpoint() {
-	// midpoint method to draw line
+void rst::rasterizer::drawLine_midpoint(Eigen::Vector3f begin, Eigen::Vector3f end, Eigen::Vector3f line_color = {255, 255, 255}) {
+	// midpoint method to draw line 
+	// note we need to convert any line into a canonical 
+	// form: 1. abs(x2 - x1) > abs(y2 - y1) 2. x2 > x1 3. slope is okay if its' < 0.	
+    auto x1 = begin.x();
+    auto y1 = begin.y();
+    auto x2 = end.x();
+    auto y2 = end.y();	
+	bool steep = false;
+	// rule 1
+	if (std::abs(x2 - x1) < std::abs(y2 - y1)) {
+		steep = true;
+		std::swap(x1, y1);
+		std::swap(x2, y2);		
+	}
+
+	// rule 2
+	if (x1 > x2) {
+		std::swap(x1, x2);
+		std::swap(y1, y2);
+	}	
+	float slope = std::abs((y2 - y1) / (x2 - x1));
+	const int yincrs = y2 > y1? 1: -1; // increase / derease of y
+	Eigen::Vector2i point = Eigen::Vector2i; // next line point position
+
+	// Reference: https://www.geeksforgeeks.org/mid-point-line-generation-algorithm/
+	int y = y1;						  // y of the initial selected point
+	int dy = y2 - y1, dx = x2 - x1;   // difference in y/x
+	int m = dy - dx / 2; 			  // midpoint's representation in f(x,y)
+
+	if (steep) {		
+		for (int x = x1; x <= x2; x++) {
+			point[0] = y, point[1] = x;
+			set_pixel(point, line_color);
+			if (m < 0) 				  // mid point is above line. Choose E as the next point.
+				d = d + dy;
+			else {
+				d = d + dy - dx; 	  // mid point is below line. Choose NE as the next point.
+				y = y + yincrs;				
+			}
+		}
+	}
+
+	else {
+		for (int x = x1; x <= x2; x++) {
+			point[0] = x, point[1] = y;
+			set_pixel(point, line_color);
+			if (m < 0) 				  // mid point is above line. Choose E as the next point.
+				d = d + dy;
+			else {
+				d = d + dy - dx; 	  // mid point is below line. Choose NE as the next point.
+				y = y + yincrs;				
+			}
+		}
+	}	
 }
 
 void rst::rasterizer::drawTriangle() {
